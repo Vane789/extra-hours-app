7fcb8462d8e50cda692c0989c6c443d2b8f46da2
Vane789 <anlly.cardona@genstudents.org>
2024-11-06 16:43:35 -0500

    configuracion cors

---
+++ backend/src/main/java/com/example/api_gestion_horas_extra/Config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.api_gestion_horas_extra.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.lang.NonNul
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
