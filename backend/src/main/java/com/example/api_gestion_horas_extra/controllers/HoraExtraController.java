53f81aecef79b2525b800f8e69e63c20fc765c19
Vane789 <anlly.cardona@genstudents.org>
2024-11-06 16:59:26 -0500

    errores

--- backend/src/main/java/com/example/api_gestion_horas_extra/controllers/HoraExtraController.java
+++ backend/src/main/java/com/example/api_gestion_horas_extra/controllers/HoraExtraController.java
@@ -1,7 +1,7 @@
 package com.example.api_gestion_horas_extra.controllers;
 
-import com.example.api_gestion_horas_extra.Model.HoraExtra;
-import com.example.api_gestion_horas_extra.Service.HoraExtraService;
+import com.example.api_gestion_horas_extra.models.HoraExtra;
+import com.example.api_gestion_horas_extra.services.HoraExtraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
