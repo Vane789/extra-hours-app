70d2075385c4b4beaa15c893e0c3d4d009ff009f
Vane789 <anlly.cardona@genstudents.org>
2024-11-14 18:19:06 -0500

    agrego entity

---
+++ backend/src/main/java/com/example/api_gestion_horas_extra/entity/ExtraHours.java
@@ -0,0 +1,4 @@
+package com.example.api_gestion_horas_extra.entity;
+
+public class ExtraHours {
+}
--- backend/src/main/java/com/example/api_gestion_horas_extra/repositories/HoraExtraRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.api_gestion_horas_extra.repositories;
-
-
-import com.example.api_gestion_horas_extra.models.HoraExtra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HoraExtraRepository extends JpaRepository<HoraExtra, Long> {
-}
