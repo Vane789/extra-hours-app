4e70cb4fb2733dbd3df2e11a1d14142432efc9a2
Vane789 <anlly.cardona@genstudents.org>
2024-11-15 11:51:49 -0500

    se arreglan estilos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
-import { useContext } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ExtraHoursMenu from "./components/ExtraHoursMenu";
 import LoginPage from "./components/auth/LoginPage";
 import ReportsPage from "./pages/ReportsPage";
@@ -9,80 +10,92 @@ import ApprovePage from "./pages/ApprovePage";
 import AddExtrahour from "./pages/AddExtrahour";
 import PayExtraHoursPage from "./pages/PayExtraHoursPage";
 import DeleteExtrahour from "./pages/DeleteExtrahour";
-import { AuthProvider, AuthContext } from './components/context/AuthContext';
+import { AuthProvider, AuthContext } from "./components/context/AuthContext";
 import Footer from "./components/common/Footer";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './index.scss';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.scss";
 
 const ProtectedRoute = ({ children, roles = [] }) => {
   const { auth, isAuthenticated } = useContext(AuthContext);
 
   if (!isAuthenticated()) {
-      return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />;
   }
 
   if (roles.length > 0 && !roles.includes(auth.role)) {
-      return <Navigate to="/menu" replace />;
+    return <Navigate to="/menu" replace />;
   }
 
   return children;
 };
 
 const ROUTE_ROLES = {
-  '/add': ['USER', 'ADMIN'],
-  '/reports': ['ADMIN'],
-  '/approve-payroll': ['ADMIN'],
-  '/update': ['ADMIN'],
-  '/delete': ['ADMIN'],
-  '/settings': ['ADMIN']
+  "/add": ["USER", "ADMIN"],
+  "/reports": ["ADMIN"],
+  "/approve-payroll": ["ADMIN"],
+  "/update": ["ADMIN"],
+  "/delete": ["ADMIN"],
+  "/settings": ["ADMIN"],
 };
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
+  console.log(isLoginPage); 
   return (
-    <BrowserRouter>
-    <ToastContainer />
+    <>
+      <ToastContainer />
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          
-          <Route 
-            path="/menu" 
-            element={
-              <ProtectedRoute roles={['USER', 'ADMIN']}>
-                <ExtraHoursMenu />
-              </ProtectedRoute>
-            } 
-          />
+        <div className="main-content">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
 
-          {Object.entries(ROUTE_ROLES).map(([path, roles]) => (
             <Route
-              key={path}
-              path={path}
+              path="/menu"
               element={
-                <ProtectedRoute roles={roles}>
-                  {(() => {
-                    switch (path) {
-                      case '/add': return <AddExtrahour />;
-                      case '/reports': return <ReportsPage />;
-                      case '/approve-payroll': return <ApprovePage />;
-                      case '/update': return <PayExtraHoursPage />;
-                      case '/delete': return <DeleteExtrahour />;
-                      case '/settings': return <SettingsPage />;
-                      default: return null;
-                    }
-                  })()}
+                <ProtectedRoute roles={["USER", "ADMIN"]}>
+                  <ExtraHoursMenu />
                 </ProtectedRoute>
               }
             />
-          ))}
 
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
+            {Object.entries(ROUTE_ROLES).map(([path, roles]) => (
+              <Route
+                key={path}
+                path={path}
+                element={
+                  <ProtectedRoute roles={roles}>
+                    {(() => {
+                      switch (path) {
+                        case "/add":
+                          return <AddExtrahour />;
+                        case "/reports":
+                          return <ReportsPage />;
+                        case "/approve-payroll":
+                          return <ApprovePage />;
+                        case "/update":
+                          return <PayExtraHoursPage />;
+                        case "/delete":
+                          return <DeleteExtrahour />;
+                        case "/settings":
+                          return <SettingsPage />;
+                        default:
+                          return null;
+                      }
+                    })()}
+                  </ProtectedRoute>
+                }
+              />
+            ))}
+
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </div>
       </AuthProvider>
-      <Footer/>
-    </BrowserRouter>
+      <Footer className={isLoginPage ? "footer footer-fixed" : "footer"} />
+    </>
   );
 }
 
