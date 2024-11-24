edf257929d4503a7da360bbf77d0baaa68b28ad4
Vane789 <vane789vane@hotmail.com>
2024-11-24 08:37:21 -0500

    se crea incidentDTO y se modifica coódigo

--- backend/src/main/java/com/example/api_gestion_horas_extra/dto/IncidentDTO.java
+++ backend/src/main/java/com/example/api_gestion_horas_extra/dto/IncidentDTO.java
@@ -1,2 +1,17 @@
-package com.example.api_gestion_horas_extra.dto;public class IncidentDTO {
+package com.example.api_gestion_horas_extra.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IncidentDTO {
+    private Integer id;
+    private String description;
+    private LocalDateTime createdAt;
 }
