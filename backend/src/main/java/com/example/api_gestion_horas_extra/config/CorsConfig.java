7723e34d922c56fc130f06098fbe199ff4963467
Vane789 <anlly.cardona@genstudents.org>
2024-12-05 19:33:24 -0500

    deploy

--- backend/src/main/java/com/example/api_gestion_horas_extra/config/CorsConfig.java
+++ backend/src/main/java/com/example/api_gestion_horas_extra/config/CorsConfig.java
@@ -2,6 +2,11 @@ package com.example.api_gestion_horas_extra.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.lang.NonNull;
@@ -15,7 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedOrigins("https://extra-hours-app.vercel.app")
                         .allowedHeaders("*")
                         .allowedHeaders("Authorization", "*")
@@ -24,4 +29,5 @@ public class CorsConfig {
             }
         };
     }
+
 }
